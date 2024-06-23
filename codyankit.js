ea30c04859077f03571e8fac99fb144ec5e010d7
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-06-24 00:35:14 +0530

    first commit

---
+++ codyankit.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector(".navbar")
+const cartItems = document.querySelector(".cart-items-container")
+const searchForm = document.querySelector(".search-form")
+
+document.getElementById('menu-btn').addEventListener('click',()=>{
+    navbar.classList.toggle('active')
+    cartItems.classList.remove('active')
+    searchForm.classList.remove('active')
+})
+
+document.getElementById('cart-btn').addEventListener('click',()=>{
+    navbar.classList.remove('active')
+    cartItems.classList.toggle('active')
+    searchForm.classList.remove('active')
+})
+
+document.getElementById('search-btn').addEventListener('click',()=>{
+    navbar.classList.remove('active')
+    cartItems.classList.remove('active')
+    searchForm.classList.toggle('active')
+})
+
+window.onscroll = () => {
+    navbar.classList.remove('active')
+    cartItems.classList.remove('active')
+    searchForm.classList.remove('active')
+}
